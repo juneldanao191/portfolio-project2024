8f36f68a88889fcae9906026c69bc31c2bb2eed3
Junel Danao <jhonezdanao@gmail.com>
2024-02-29 23:57:26 -0800

    Design Banners.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import Logo from '../assets/logo.png';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className='py-8'>
+      <div className='container mx-auto'>
+        <div className='flex justify-between items-center'>
+          <a href='#'>
+            <img src={Logo} alt=''>
+
+            </img>
+
+          </a>
+          <button className='btn btn-sm'>Work with me</button>
+        </div>
+      </div>
+    </header>
+
+  );
+
 };
 
 export default Header;
